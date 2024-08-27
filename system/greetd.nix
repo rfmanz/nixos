f57e081052da4591d407fa164f25ff42a33f4be5
Rafael <rafael.arevalo27@gmail.com>
2024-08-27 01:46:00 -0500

    .

---
+++ system/greetd.nix
@@ -0,0 +1,19 @@
+{ pkgs, systemSettings, ... }:
+
+{
+  # Login manager
+  services.greetd = {
+    enable = true;
+    restart = false;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = systemSettings.username;
+      };
+      initial_session = {
+        command = "Hyprland";
+        user = systemSettings.username;
+      };
+    };
+  };
+}
